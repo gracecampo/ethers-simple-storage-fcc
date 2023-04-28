9c282d8230cb9883fbd3882844af4358c0472ef3
cainiaojin <741143592@qq.com>
2023-04-28 15:57:00 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch typescript # Changes to be committed: #	renamed:    deploy.js -> deploy.ts #	renamed:    encryptKey.js -> encryptKey.ts #	modified:   package.json #

--- deploy.ts
+++ deploy.ts
@@ -1,15 +1,15 @@
-const ethers = require("ethers");
-const fs = require("fs-extra")
-require("dotenv").config();
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main(){
-    const provider =new ethers.JsonRpcProvider(process.env.RPC_URL)
+    const provider =new ethers.providers.JsonRpcProvider(process.env.RPC_URL!)
     // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY,provider);
     const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
     const password  = process.env.PRIVATE_KEY_PASSOWRD;
     let wallet = ethers.Wallet.fromEncryptedJsonSync(
       encryptedJson,
-      password
+      password!
     );
     wallet = wallet.connect(provider);
     //deploy contract
--- encryptKey.ts
+++ encryptKey.ts
@@ -1,9 +1,9 @@
-const ethers = require("ethers")
-const fs = require("fs-extra")
-require("dotenv").config()
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main(){
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!)
     
     // const encryptedJsonKey = await wallet.encrypt(
     //     process.env.PRIVATE_KEY_PASSWORD,
@@ -12,7 +12,7 @@ async function main(){
     // In later version (^6.2.3 as of this commit) of etherjs, PRIVATE_KEY is inferred from wallet, so there is no need to 
     // pass private key again. 
         const encryptedJsonKey = await wallet.encrypt(
-            process.env.PRIVATE_KEY_PASSOWRD,
+            process.env.PRIVATE_KEY_PASSOWRD!,
      )
     console.log(encryptedJsonKey)
     fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
